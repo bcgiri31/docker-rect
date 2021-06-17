c0058631323110d1d8d0c40a0fa8672794b5ee85
bcgiri31 <b.c.giri31@gmail.com>
2021-06-17 15:42:01 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        learn react Hi there ram!!$ ha ha ha
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
